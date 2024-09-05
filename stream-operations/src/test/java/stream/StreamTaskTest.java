f29574daa4dce936976fa4d64e8c2f7c5ef34454
AlexanderBaranov <avbc@mail.ru>
2024-09-05 22:43:04 +0300

    add empty

--- stream-operations/src/test/java/stream/StreamTaskTest.java
+++ stream-operations/src/test/java/stream/StreamTaskTest.java
@@ -7,10 +7,13 @@ public class StreamTaskTest {
     void shuffleTest() {
         int[] arr = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         int[] actualArr = StreamTasks.shuffle(arr);
-
+        int[] arr1 = {};
+        
         assertFalse(Arrays.equals(arr, actualArr));
         for (int i: actualArr) {
             assertTrue(Arrays.binarySearch(arr, i) >= 0);
         }
+        assertThrowsExactly(IllegalArgumentException.class, () -> StreamTasks.shuffle(arr1));
     }
+    
 }
