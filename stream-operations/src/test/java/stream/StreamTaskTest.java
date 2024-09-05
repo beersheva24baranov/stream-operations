a9c8bb0fcd7002ecaa899e5977545012a3716202
AlexanderBaranov <avbc@mail.ru>
2024-09-05 22:49:28 +0300

    test

--- stream-operations/src/test/java/stream/StreamTaskTest.java
+++ stream-operations/src/test/java/stream/StreamTaskTest.java
@@ -8,12 +8,13 @@ public class StreamTaskTest {
         int[] arr = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         int[] actualArr = StreamTasks.shuffle(arr);
         int[] arr1 = {};
-        
+        int[] actualArr1 = StreamTasks.shuffle(arr1);
+
         assertFalse(Arrays.equals(arr, actualArr));
         for (int i: actualArr) {
             assertTrue(Arrays.binarySearch(arr, i) >= 0);
         }
-        assertThrowsExactly(IllegalArgumentException.class, () -> StreamTasks.shuffle(arr1));
+        assertThrowsExactly(IllegalArgumentException.class, () -> StreamTasks.shuffle(start4));
     }
     
 }
